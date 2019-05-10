d77db7bb3e7f02bd2b99db74876ab1578947302b
mattbf <matthewbf8@gmail.com>
2019-05-10 14:35:15 -0600

    Created template for requesting all available and calculated ddata for a stock symnbol

--- pythonCode/getDataV2.py
+++ pythonCode/getDataV2.py
@@ -1,89 +1,91 @@
-# Getting Alpha Vantage data using the alphavantage python package
-# from alpha_vantage.timeseries import TimeSeries
-# ts = TimeSeries(key='EJ69MPM068NGTJ30', output_format='pandas')
-# data, meta_data = ts.get_intraday(
-#     symbol='MSFT', interval='1min', outputsize='compact')
-# print(data)
-
-# Making request to the Alpha vantage API using the request python package
 import json
 import requests
-import datetime
+from datetime import datetime
 
 api_token = 'B62IP93O6OGM4LCA'  # API token from alpha vantage
 api_url_base = 'https://www.alphavantage.co/query?'
-# ex. function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=5min&apikey=demo'
-#  api_url = '{}orgs/{}/repos'.format(api_url_base, username)
-
-# payload = {'key1': 'value1', 'key2': 'value2'}
-# r = requests.get('https://httpbin.org/get', params=payload)
 
 
-def format_data(json, selector):
+def format_data(json, selector, dateFormat):
     newObj = []
-    for obj in json.data[selector]:
-        for key in obj.key():
-            newObj.append({
-                'date': datetime.datetime(datetime.strptime(key, '%b %d %Y %I:%M%p')),
-                'open': json.data["1. open"],
-                'close': json.data["4. close"],
-                'volume': json.data["5. volume"],
-            })
+    # print(json[selector])
+    for key in json[selector]:
+        # print(key)
+        newObj.append({
+            # (datetime.strptime(key, '%Y-%m-%d %H:%M:%S'))
+            # (datetime.strptime(key, dateFormat))
+            'date': key,
+            'open': json[selector][key]["1. open"],
+            'close': json[selector][key]["4. close"],
+            'volume': json[selector][key]["5. volume"],
+        })
     return newObj
 
 
 def get_series_data(symbol, apikey):
 
-    # api_url = '{0}account'.format(api_url_base)
-    # outputsize=compact change to full for all data
-    # change to TIME_SERIES_DAILY
-    parameters = {'function': 'TIME_SERIES_INTRADAY',
-                  'symbol': 'MSFT', 'interval': '5min', 'apikey': 'demo'}
-    # response = requests.get(api_url_base, params=parameters)
-
     dailyParams = {
-        'symbol': 'MSFT',
+        'symbol': symbol,
         'function': 'TIME_SERIES_INTRADAY',
         'interval': '5min',
-        'apikey': 'demo',
+        'apikey': apikey,
+        'outputsize': 'compact',
     }
 
     historicParams = {
-        'key': 'value',
+        'symbol': symbol,  # request.query_params.symbol
+        'function': 'TIME_SERIES_DAILY',
+        'apikey': apikey,
+        'outputsize': 'compact',
     }
 
     dailyData = requests.get(
         'https://www.alphavantage.co/query?',
-        params=request.query_params
+        params=dailyParams
     )
-    dailyFormated = format_data(json.loads(dailyData.content.decode('utf-8')))
+    dailyFormated = format_data(
+        json.loads(dailyData.content.decode('utf-8')),
+        "Time Series (5min)",
+        '%Y-%m-%d %H:%M:%S'
+    )
 
     historicData = requests.get(
         'https://www.alphavantage.co/query?',
-        params=request.query_params
+        params=historicParams
     )
-    historicFormated = format_data(json.loads(historicData.content.decode('utf-8')))
+    historicFormated = format_data(
+        json.loads(historicData.content.decode('utf-8')),
+        "Time Series (Daily)",
+        '%Y-%m-%d'
+    )
 
     # Make calcs, categorize time data into slices,
     # add kpis, and package all together
 
-    return Response({'data': json.loads(data.content.decode('utf-8')),
-                     'request': {'method': request.method,
-                                 'path': request.path,
-                                 'params': request.query_params,
-                                 },
-                     })
+    # return Response({'data': json.loads(data.content.decode('utf-8')),
+    #                  'request': {'method': request.method,
+    #                              'path': request.path,
+    #                              'params': request.query_params,
+    #                              },
+    #                  })
+    Response = {
+        'data': {
+            'daily': dailyFormated,
+            'historic': historicFormated,
+        },
+        'kpis': {
+            'PE': 5,
+        }
 
-    response = requests.get(
-        'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=5min&apikey=demo')
-    if response.status_code == 200:
-        return Response({'data': json.loads(data.content.decode('utf-8')),
-                         })
+    }
+
+    if historicData.status_code == 200 and dailyData.status_code == 200:
+        return Response
     else:
         return None
 
 
-data = get_series_data(symbol, apikey)
+data = get_series_data('MSFT', 'B62IP93O6OGM4LCA')
 
 if data is not None:
     print("Here's your info: ")
@@ -92,3 +94,14 @@ if data is not None:
     #     print('{0}:{1}'.format(k, v))
 else:
     print('[!] Request Failed')
+
+    # formating logic
+    #
+    # for obj in json[selector]:
+    #     for key in obj.key():
+    #         newObj.append({
+    #             'date': datetime.datetime(datetime.strptime(key, '%b %d %Y %I:%M%p')),
+    #             'open': json["1. open"],
+    #             'close': json["4. close"],
+    #             'volume': json["5. volume"],
+    #         })
