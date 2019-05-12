9548568b46e47c8b89075dd020fadcd98f171465
mattbf <matthewbf8@gmail.com>
2019-05-12 08:18:55 -0600

    created stockt ticket kpi card

--- frontend/src/StockKPI.js
+++ frontend/src/StockKPI.js
@@ -30,18 +30,28 @@ const useStyles = makeStyles((theme: Theme) =>
       paddingRight: theme.spacing(2),
       position: 'relative',
       minWidth: '125px',
-      height: '75px',
+      height: '100px',
       margin: theme.spacing(1),
     },
     Heading: {
       marginRight: '20px',
     },
     Value: {
-
+      marginRight: '5px',
+      color: upordown ? '#00c676' : '#ff1744',
+    },
+    percent: {
+      color: upordown ? '#00c676' : '#ff1744'
+    },
+    arrow: {
+      color: upordown ? '#00c676' : '#ff1744',
+      marginBottom: '2px',
     },
     valuebox: {
       display: 'flex',
       alignItems: 'center',
+      justifyContent: 'center',
+      marginBottom: '5px',
     },
     margin: {
       margin: theme.spacing(1),
@@ -54,9 +64,11 @@ const useStyles = makeStyles((theme: Theme) =>
     valueHeader: {
       display: 'flex',
       alignItems: 'bottom',
-    }
+    },
   }),
 );
+
+const upordown = true
 
 function KPI(info) {
   const classes = useStyles();
@@ -65,13 +77,19 @@ function KPI(info) {
     <div className={classes.root}>
       <Paper className={classes.card}>
         <div className={classes.valueHeader}>
-          <Typography variant='h5' className={classes.Heading}> Value{info.name} </Typography>
+          <Typography variant='h5' className={classes.Heading}> 101.57{info.name} </Typography>
           <div className={classes.valuebox}>
-            <Typography variant='subtitle1' className={classes.Value}> diff{info.prefix} </Typography>
-            <Typography variant='subtitle1' className={classes.Value}> perc%{info.value} </Typography>
-            <Typography variant='subtitle1' className={classes.Value}> {info.suffix} </Typography>
+            <Typography variant='subtitle1' className={classes.percent}> {upordown ? '+' : '-'}</Typography>
+            <Typography variant='subtitle1' className={classes.Value}> 2.01{info.prefix} </Typography>
+            <Typography variant='subtitle1' className={classes.percent}> ({info.prefix} </Typography>
+            <Typography variant='subtitle1' className={classes.percent}> 8.09{info.value} </Typography>
+            <Typography variant='subtitle1' className={classes.percent}> %){info.suffix} </Typography>
+            {upordown ? <ArrowUpward style={{ fontSize: 20 }} className={classes.arrow}/> : <ArrowDownward style={{ fontSize: 20 }} className={classes.arrow}/> }
+
           </div>
         </div>
+        <Typography variant='subtitle1'> SYBL{info.suffix} </Typography>
+        <Typography variant='body2' className={classes.companyText}> Company Name could be long{info.suffix} </Typography>
       </Paper>
     </div>
   )
